1386cda5a653a559f1a457a95457644c6320bb19
Christian Villamin <cvillamin@joyride.com.ph>
2024-03-30 12:43:04 +0800

    feat: user model actions

--- src/db/users.ts
+++ src/db/users.ts
@@ -11,3 +11,31 @@ const UserSchema = new mongoose.Schema({
 });
 
 export const UserModel = mongoose.model("User", UserSchema);
+
+// -- Actions
+export const getUsers = () => {
+  return UserModel.find();
+};
+export const getUserByEmail = (email: string) => {
+  return UserModel.findOne({ email });
+};
+
+export const getUserBySessionToken = (sessionToken: string) => {
+  return UserModel.findOne({ "authentication.sessionToken": sessionToken });
+};
+
+export const getUserById = (id: string) => {
+  return UserModel.findById(id);
+};
+
+export const createUser = async (values: Record<string, any>) => {
+  return new UserModel(values).save().then((user) => user.toObject());
+};
+
+export const deleteUserById = (id: string) => {
+  return UserModel.findOneAndDelete({ _id: id });
+};
+
+export const updateUserbyId = (id: string, values: Record<string, any>) => {
+  return UserModel.findByIdAndUpdate(id, values);
+};
