586636428bbefcea2e9c6844f3aba5cfb4730710
Christian Villamin <cvillamin@joyride.com.ph>
2024-03-30 12:37:29 +0800

    feat: mongoose init

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,7 @@ import bodyParser from "body-parser";
 import cookieParser from "cookie-parser";
 import compression from "compression";
 import cors from "cors";
+import mongoose, { mongo } from "mongoose";
 
 const app = express();
 
@@ -18,3 +19,10 @@ const server = http.createServer(app);
 server.listen(8080, () => {
   console.log(`Server running on http://localhost:8080/`);
 });
+
+const MONGO_URL =
+  "mongodb+srv://codekcv:codekcv@cluster0.usddpuy.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+
+mongoose.Promise = Promise;
+mongoose.connect(MONGO_URL);
+mongoose.connection.on("error", (error: Error) => console.error(error));
