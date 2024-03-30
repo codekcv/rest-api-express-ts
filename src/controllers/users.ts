441d385e354a54ce8d47cfc6ce59166b41075c90
Christian Villamin <cvillamin@joyride.com.ph>
2024-03-30 21:58:00 +0800

    feat: get, delete, update user

---
+++ src/controllers/users.ts
@@ -0,0 +1,46 @@
+import { deleteUserById, getUserById, getUsers } from "@src/db/users";
+import { Request, Response } from "express";
+
+export async function getAllUsers(req: Request, res: Response) {
+  try {
+    const users = await getUsers();
+
+    return res.status(200).json(users);
+  } catch (error) {
+    console.log(error);
+    return res.sendStatus(400);
+  }
+}
+
+export async function deleteUser(req: Request, res: Response) {
+  try {
+    const { id } = req.params;
+    const deletedUser = await deleteUserById(id);
+
+    return res.json(deletedUser);
+  } catch (error) {
+    console.log(error);
+    return res.sendStatus(400);
+  }
+}
+
+export async function updateUser(req: Request, res: Response) {
+  try {
+    const { id } = req.params;
+    const { username } = req.body;
+
+    if (!username) {
+      return res.sendStatus(400);
+    }
+
+    const user = await getUserById(id);
+
+    user.username = username;
+    await user.save();
+
+    return res.status(200).json(user).end();
+  } catch (error) {
+    console.log(error);
+    return res.sendStatus(400);
+  }
+}
